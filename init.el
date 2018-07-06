542872f42a24314dd6e6da270ae75c88a38b92df
Jack Crowson <crowstar@localhost.localdomain>
2018-07-06 18:50:31 +0100

    initial commit

---
+++ init.el
@@ -0,0 +1,54 @@
+;; package setups
+(require 'package)
+
+;; Add melpa package source when using package list
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+;; Load emacs packages and activate them
+;; This must come before configurations of installed packages.
+;; Don't delete this line.
+(package-initialize)
+
+;; line nos
+(add-hook 'prog-mode-hook 'linum-mode)
+
+;; theme
+(load-theme 'solarized-dark t)
+
+;;
+;; ORG MODE SETTINGS
+;;
+;; Enable Org mode
+(require 'org)
+
+;; use C-c a for agendas
+(global-set-key "\C-ca" 'org-agenda)
+
+;; auto close brackets etc.
+(electric-pair-mode 1)
+
+;; emacs gui stuff
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (solarized-theme)))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
