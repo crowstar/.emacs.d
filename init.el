7761c0aaf4d9ad72bd71815099cec2924c2574d6
Jack Crowson <crowstar@localhost.localdomain>
2018-07-07 18:38:45 +0100

    add useful packages: magit, jedi etc.

--- init.el
+++ init.el
@@ -1,28 +1,61 @@
 ;; package setups
 (require 'package)
 
-;; Add melpa package source when using package list
+;; Add package sources when using package list
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 
 ;; Load emacs packages and activate them
 ;; This must come before configurations of installed packages.
 ;; Don't delete this line.
 (package-initialize)
 
+;; Check if use-package is installed
+;; install if not
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; Check if packages need updating
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
 ;; line nos
 (add-hook 'prog-mode-hook 'linum-mode)
 
 ;; theme
-(load-theme 'solarized-dark t)
+(use-package solarized-theme)
 
 ;; auto-closing brackets
 (electric-pair-mode 1)
+
+;; Jedi.el for python autocomplete
+(use-package jedi
+  :ensure t
+  :init
+  (setq jedi:setup-keys t)
+  (add-hook 'python-mode 'jedi:setup)
+  (add-hook 'python-mode 'jedi:ac-setup))
+
+;; Magit settings
+(use-package magit
+  :bind ("C-x g" . 'magit-status)
+  :ensure t)
 
 ;;
 ;; ORG MODE SETTINGS
 ;;
 ;; Enable Org mode
-(require 'org)
+(use-package org
+  :ensure t)
 
 ;; use C-c a for agendas
 (global-set-key "\C-ca" 'org-agenda)
@@ -46,7 +79,7 @@
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (solarized-theme)))
+ '(package-selected-packages (quote (auto-package-update solarized-theme)))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
